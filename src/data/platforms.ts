a09d0f54201a2be9634dec6ccff986335d246089
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-27 22:41:35 +0100

    add playstation console

--- src/data/platforms.ts
+++ src/data/platforms.ts
@@ -7,6 +7,7 @@ type Platforms = {
 }[];
 
 const {
+  // -- Nintendo --
   Gameboy,
   Nintendo64,
   Nintendo3DS,
@@ -15,10 +16,17 @@ const {
   NintendoGameCube,
   GameboyAdvance,
   GameboyColor,
+  // -- Xbox --
   Xbox360,
+  Xbox,
+  // -- PlayStation --
+  PS1,
+  PS2,
+  PS3,
+  PSV,
 } = EmulatorTypes;
 
-const { Nintendo, Xbox } = BrandTypes;
+const { Nintendo, Xbox: Microsoft, PlayStaion } = BrandTypes;
 
 export const platforms: Platforms = [
   {
@@ -35,7 +43,11 @@ export const platforms: Platforms = [
     ],
   },
   {
-    name: Xbox,
-    children: [Xbox360],
+    name: Microsoft,
+    children: [Xbox360, Xbox],
+  },
+  {
+    name: PlayStaion,
+    children: [PS1, PS2, PS3, PSV],
   },
 ];
