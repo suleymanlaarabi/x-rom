bd437aab858a551c8441b133ae4647751fa81c77
Suleyman Laarabi <suleyman.laarabi.dev@gmail.com>
2023-11-28 01:02:18 +0100

    improve RomeCard UI

--- src/components/common/Card/RomCard.tsx
+++ src/components/common/Card/RomCard.tsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import { Rom } from "../../../types/RomsTypes";
 
 import {
@@ -8,65 +9,104 @@ import {
   Button,
   useColorModeValue,
   Flex,
+  List,
+  ListItem,
 } from "@chakra-ui/react";
 
-export default function RomCard({
-  name,
-  platforms,
-  file_size,
-  download_links,
-}: Rom) {
-  return (
-    <Center height={"450px"} py={6}>
-      <Box
-        maxW={"330px"}
-        height={"100%"}
-        w={"full"}
-        bg={useColorModeValue("white", "gray.800")}
-        boxShadow={"2xl"}
-        rounded={"md"}
-        overflow={"hidden"}
-      >
-        <Stack
-          height={"65%"}
-          textAlign={"center"}
-          p={6}
-          color={useColorModeValue("gray.800", "white")}
-          align={"center"}
-        >
-          <Text fontSize={"sm"} fontWeight={500} p={2} px={3} rounded={"full"}>
-            {platforms}
-          </Text>
-          <Stack direction={"row"} align={"center"} justify={"center"}>
-            <Text fontSize={"3xl"} fontWeight={800}>
-              {name.slice(0, 35)}
-              {name.length >= 35 && "..."}
-            </Text>
-          </Stack>
-        </Stack>
+const RomCard = memo(
+  ({
+    name,
 
-        <Flex
-          flexDirection={"column"}
-          height={"35%"}
-          bg={useColorModeValue("gray.50", "gray.900")}
-          px={10}
-          py={5}
-        >
-          <Text>{file_size}</Text>
+    file_size,
+    download_links,
+  }: Rom) => {
+    let filteredName = name;
 
-          <Button
-            mt={3}
-            w={"full"}
-            minH={35}
-            rounded={"xl"}
-            onClick={() => {
-              window.open(download_links);
-            }}
+    // Utilisation d'une expression régulière pour extraire tous les textes entre parenthèses
+    const correspondances = filteredName.match(/\((.*?)\)/g);
+
+    // Tableau pour stocker les textes entre parenthèses
+    const textesEntreParentheses: string[] = [];
+
+    // Si des correspondances sont trouvées, les stocker dans le tableau et les supprimer du titre
+    if (correspondances) {
+      correspondances.forEach((correspondance) => {
+        const texteEntreParentheses = correspondance.slice(1, -1); // Supprimer les parenthèses
+        textesEntreParentheses.push(texteEntreParentheses);
+        filteredName = filteredName.replace(correspondance, "").trim();
+      });
+    }
+
+    return (
+      <Center w={"330px"} height={"510px"} py={6}>
+        <Box
+          maxW={"330px"}
+          height={"100%"}
+          w={"full"}
+          bg={useColorModeValue("white", "gray.800")}
+          boxShadow={"2xl"}
+          rounded={"md"}
+          overflow={"hidden"}
+        >
+          <Stack
+            height={"60%"}
+            textAlign={"center"}
+            p={6}
+            color={useColorModeValue("gray.800", "white")}
+            align={"center"}
           >
-            Download
-          </Button>
-        </Flex>
-      </Box>
-    </Center>
-  );
-}
+            <Text
+              bg={useColorModeValue("gray.200", "gray.600")}
+              fontSize={"sm"}
+              fontWeight={500}
+              p={2}
+              px={3}
+              rounded={"full"}
+            >
+              {file_size}
+            </Text>
+            <Stack direction={"row"} align={"center"} justify={"center"}>
+              <Text fontSize={"3xl"} fontWeight={800}>
+                {filteredName.slice(0, 35)}
+                {filteredName.length >= 35 && "..."}
+              </Text>
+            </Stack>
+          </Stack>
+
+          <Flex
+            flexDirection={"column"}
+            justifyContent={"space-between"}
+            height={"40%"}
+            bg={useColorModeValue("gray.50", "gray.900")}
+            px={10}
+            py={5}
+          >
+            <List>
+              {textesEntreParentheses.map((el, index) => {
+                return (
+                  <ListItem key={index}>
+                    <Text>{el}</Text>
+                  </ListItem>
+                );
+              })}
+            </List>
+
+            <Button
+              mt={3}
+              w={"full"}
+              minH={35}
+              rounded={"xl"}
+              onClick={() => {
+                window.open(download_links);
+              }}
+            >
+              Download
+            </Button>
+          </Flex>
+        </Box>
+      </Center>
+    );
+  }
+);
+
+export default RomCard;
